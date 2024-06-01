858332285283657fae9e8e1f698b8775cf89318b
yosukeOpendna <murata@open-dna.jp>
2024-06-01 13:00:31 +0900

    Initial Commit

---
+++ ChatDemo/ChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatDemoApp.swift
+//  ChatDemo
+//
+//  Created by ISHOWSPEED on 2024/06/01.
+//
+
+import SwiftUI
+
+@main
+struct ChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
